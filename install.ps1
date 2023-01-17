b5854bc2d96d9b0cecb23a90ee73f12f7e42a872
Jayakumar6 <95211511+Jayakumar6@users.noreply.github.com>
2023-01-17 21:24:06 +0530

    Create install.ps1

---
+++ install.ps1
@@ -0,0 +1,2 @@
+New-Item -Path 'C:\Users\adminuser\Downloads\test' -ItemType Directory
+Invoke-WebRequest -Uri "https://tallymirror.tallysolutions.com/download_centre/Rel_2.1/TP/Full/setup.exe" -OutFile "C:\Users\adminuser\Downloads\setup.exe"
